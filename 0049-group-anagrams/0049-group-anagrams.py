a5852f7785dbeca3cd6e625a7276e5b5987d0bfc
Nayoon <nayoonk928@gmail.com>
2024-01-03 17:53:04 +0900

    Time: 118 ms (18.08%), Space: 20.6 MB (46.63%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,3 @@
-from typing import List
-from collections import defaultdict
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram_groups = defaultdict(list)

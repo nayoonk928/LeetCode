7d1a4bb7f2557faef010d339d1a8978677b92a67
Nayoon <nayoonk928@gmail.com>
2024-01-05 02:22:41 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (13.28%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int index = digits.length - 1;
-        int add = 1;
+        boolean flag = false;
         
-        while (index >= 0 && add > 0) {
-            digits[index] = (digits[index] + add) % 10;
-            add = (digits[index] == 0) ? 1 : 0;
-            
-            index--;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+                flag = true;
+            } else {
+                digits[i] += 1;
+                flag = false;
+                break;
+            }
         }
         
-        // add가 1이라면 주어진 수보다 한 자리수 더 큰 수가 됨
-        if (add == 1) {
+        if (flag) {
             digits = new int[digits.length + 1];
             digits[0] = 1;
         }

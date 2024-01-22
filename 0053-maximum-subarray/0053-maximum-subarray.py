9db9f27d5a18cb7d1b7caa735bdb1401867f7820
Nayoon Kim <nayoonk928@gmail.com>
2024-01-22 17:12:08 +0900

    Time: 535 ms (82.38%), Space: 30.9 MB (59.60%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,36 +1,8 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        def divide_and_conquer(nums, left, right):
-            if left == right:
-                return nums[left]
-
-            mid = (left + right) // 2
-
-            # 왼쪽, 오른쪽, 가로지르는 중간 부분 배열에서 얻을 수 있는 최대 연속 부분 배열의 합
-            left_max = divide_and_conquer(nums, left, mid)
-            right_max = divide_and_conquer(nums, mid + 1, right)
-            cross_max = max_cross_sum(nums, left, mid, right)
-
-            return max(left_max, right_max, cross_max)
-
-        def max_cross_sum(nums, left, mid, right):
-            left_sum = float('-inf')
-            current_sum = 0
-
-            for i in range(mid, left - 1, -1):
-                current_sum += nums[i]
-                left_sum = max(left_sum, current_sum)
-
-            right_sum = float('-inf')
-            current_sum = 0
-
-            for i in range(mid + 1, right + 1):
-                current_sum += nums[i]
-                right_sum = max(right_sum, current_sum)
-
-            return left_sum + right_sum
-
-        if not nums:
-            return 0
-
-        return divide_and_conquer(nums, 0, len(nums) - 1)
+        dp = [nums[0]]
+        
+        for i in range(1, len(nums)):
+            dp.append(max(dp[i - 1] + nums[i], nums[i]))
+        
+        return max(dp)

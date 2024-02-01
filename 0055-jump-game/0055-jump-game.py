673bfec8b190c553df175866d6708df498fcee2c
Nayoon Kim <nayoonk928@gmail.com>
2024-02-02 02:50:56 +0900

    Time: 372 ms (67.50%), Space: 17.9 MB (60.18%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0
+        last_index = len(nums) - 1  
+
+        for i in range(len(nums)):
+            if i > max_reach:
+                return False
+    
+            max_reach = max(max_reach, i + nums[i])
+
+            if max_reach >= last_index:
+                return True
+    
+        return False
